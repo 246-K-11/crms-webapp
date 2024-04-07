62c24f1ef672bf23f6acc7e589b6bfa626c1f0a4
246-K-11 <kalbert246@outlook.com>
2024-04-07 15:38:40 -0400

    modified:   app/components/HomepageCards/HomeCarousel.tsx 	modified:   app/page.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import HomeLayout from "./components/HomepageCards/HomeLayout";
+import Carousel from "./components/HomepageCards/HomeCarousel";
 
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>
+        <Carousel />
+      </div>
       <HomeLayout />
     </main>
   );
