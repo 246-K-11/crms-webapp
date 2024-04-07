dc3dc8bc97f1394d03b3883e552bd5b30a6ac530
246-K-11 <kalbert246@outlook.com>
2024-04-07 13:02:48 -0400

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import HomeLayout from "./components/HomepageCards/HomeLayout";
-import Homepage from "./components/HomepageCards/HomeLayout";
-import { SearchInput } from "./components/searchbar/SearchUI";
 
 export default function Home() {
   return <HomeLayout />;
