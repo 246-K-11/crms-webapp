8752cb5c48397c7e82d3a5773ce48121a2bb672b
justbraf <dwaynecorey@gmail.com>
2024-04-14 23:38:39 -0400

    Add payments

---
+++ app/Payments/View/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ViewPayment from "../services/ViewPayment";
+
+const ViewPayments = (props: any) => {
+  return (
+    <main>
+      <div>
+        <ViewPayment props={props.searchParams} />
+      </div>
+    </main>
+  )
+};
+
+export default ViewPayments;
